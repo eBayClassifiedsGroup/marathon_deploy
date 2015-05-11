4aeaffcad99e658f3006e2042e639739736eaf15
Jonathan Colby <jcolby@team.mobile.de>
2015-05-12 00:48:41 +0200

    new class

---
+++ marathon_api.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+require 'uri'
+
+# http://www.mudskipper-solutions.com/home/how-to-send-jsonhttp-using-ruby
+
+class MarathonApi
+  
+  attr_accessor :application_json
+  
+  def initialize(url)
+  end
+
+  
+end
