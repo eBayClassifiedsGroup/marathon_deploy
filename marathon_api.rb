67bb62aec7570108f6e697912195a8cc2b2ef15d
Jonathan Colby <jcolby@team.mobile.de>
2015-05-12 16:58:04 +0200

    api work

--- marathon_api.rb
+++ marathon_api.rb
@@ -6,8 +6,20 @@ require 'uri'
 class MarathonApi
   
   attr_accessor :application_json
+  attr_reader :url, :options
   
-  def initialize(url)
+  def initialize(url, options = {})
+    @url = url    
+  end
+  
+  def post(json)
+    req = Net::HTTP::Post.new url.path
+    req.body = json
+    
+    res = Net::HTTP.start(uri.host, uri.port, :use_ssl => false) do |http|
+      http.request req
+    end
+    
   end
 
   
