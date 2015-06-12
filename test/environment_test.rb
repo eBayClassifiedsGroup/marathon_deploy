e48bb89bc729a98caab964c3ba0363942defacf6
Jonathan Colby <jcolby@team.mobile.de>
2015-06-13 01:30:55 +0200

    some environment tests

--- test/environment_test.rb
+++ test/environment_test.rb
@@ -3,7 +3,9 @@ require 'test_helper'
 class EnvironmentTest < Minitest::Test
 
   
-  def test_env
-    skip "todo"
+  def test_production_env_name
+    environment_name = "PRODUCTION"
+    environment = MarathonDeploy::Environment.new(environment_name)
+    assert(environment.is_production?, "Environment #{environment_name} should be identified as production")
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
-require 'marathon_deploy'
 require 'logger'
 require 'time'
 require 'marathon_deploy/application'
+require 'marathon_deploy/environment'
 require 'minitest/autorun'
 require 'minitest/unit'
 require 'minitest/pride'
